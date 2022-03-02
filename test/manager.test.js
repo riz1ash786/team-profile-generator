6ab98f9f35f238cf588080b4afed061d6dba6808
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-02 18:26:28 +0000

    test code for manager.test.js

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,1 +1,16 @@
+// using Manager constructor
+const Manager = require("../lib/manager");
 
+// creates new manager object
+test("creates a Manager object", () => {
+  const manager = new Manager("Rizwan", 86, "riz1ash@hotmail.co.uk", 4);
+
+  expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// gets role from getRole()
+test("gets role of manager", () => {
+  const manager = new Manager("Rizwan", 86, "riz1ash@hotmail.co.uk");
+
+  expect(manager.getRole()).toEqual("Manager");
+});
